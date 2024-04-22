13949f7b2f4f476418bf4e2fe7c7709979356f42
gauteurmathilde <mathilde.gauteur@ens.uvsq.fr>
2024-04-22 20:51:01 +0200

    amélioration fonctions

--- Projet423.py
+++ Projet423.py
@@ -17,9 +17,8 @@ def lire_fichier(file):
     return seq
 
 
-def point_inversion(seq, fen, pas):
+def ratios(seq, fen, pas):
     ratios = []
-    pts_inv = []
 
     for i in range(0, len(seq), pas):
         fenetre = seq[i:i + min(fen, len(seq) - i)]
@@ -33,30 +32,56 @@ def point_inversion(seq, fen, pas):
             ratio = 0
         ratios.extend([ratio] * min(pas, len(seq) - i))
 
-    i = 0
-    while i < len(seq) - 1:
-        pas = min(pas, len(seq) - i - 1)
-        if (ratios[i] < 0 and ratios[i + pas] > 0) or (ratios[i] > 0 and ratios[i + pas] < 0):
-            pts_inv.append((i + pas // 2, abs(ratios[i] - ratios[i + pas])))
-        i += pas
+    return ratios
 
-    return pts_inv, ratios
+
+def calcul_inv(ratios, len_fen):
+    if ratios[0] < 0:
+        point_inv = -1
+    else:
+        point_inv = 1
+    for i in range(1, len(ratios)):
+        if point_inv == -1:
+            if ratios[i] > 0:
+                counter = 0
+                for j in range(1,10*len_fen+1, len_fen):
+                    if ratios[i+j] > 0:
+                        counter += 1
+                if counter >= 8:
+                    return i
+        else:
+            if ratios[i] < 0:
+                counter = 0
+                for j in range(1,10*len_fen+1, len_fen):
+                    if ratios[i+j] < 0:
+                        counter += 1
+                if counter >= 8:
+                    return i
 
 
 def plot_graph(file):
 
     sequence = lire_fichier(file.name)
     fenetre = 8000
-    points_inv, ratios_GC = point_inversion(sequence, fenetre, fenetre // 2 + fenetre // 3)
-    position = max(points_inv, key=lambda x: x[1])[0]
-    print(position)
+    pas = 6750
+    ratios_GC = ratios(sequence, fenetre, pas)
+    point_inv = calcul_inv(ratios_GC, fenetre)
+    print(point_inv)
 
     plt.figure(figsize=(10, 6))
     plt.plot(range(len(sequence)), [ratios_GC[i] for i in range(len(sequence))], label='Ratio GC', color='blue')
-    plt.scatter(position, 0, color='red', s=200, label="Point d'inversion")
+
+    plt.scatter(point_inv+(fenetre/2), 0, color='red', s=200, label="Point d'inversion")
+    x_labels = [i for i in range(0, len(sequence), len(sequence) // 5)]  # Positions des étiquettes
+    x_labels.append(point_inv + (fenetre / 2))  # Ajouter la position du point d'inversion
+    x_labels.sort()  # Assurer que les étiquettes sont triées
+    plt.xticks(x_labels, [str(label) for label in x_labels], color='black')  # Étiquettes en noir
+
+
     plt.xlabel('Position dans la séquence')
     plt.ylabel('Ratio GC')
     plt.title("Représentation du point d'inversion par rapport au ratio GC")
+    plt.axvline(point_inv+(fenetre/2), color='r', linestyle='--', label='Premier nucléotide de la fenêtre contenant le point d\'inversion')
     plt.legend()
     plt.savefig('plot.png')
     return 'plot.png'
